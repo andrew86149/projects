5f84bc4d94f493e8e9f6de9d515fa609d9cdf8a3
Andrew <andrew@nikodimus.ru>
2026-03-13 18:23:38 +0300

    pacwoman

---
+++ pacwoman/Game.cpp
@@ -0,0 +1,78 @@
+#include "Game.hpp"
+#include <iostream>
+
+Game::Game()
+:m_window(sf::VideoMode({640, 480}), "Pac Woman")
+,m_font("assets/font.ttf"),m_logo("assets/logo.png"),m_texture("assets/texture.png")
+{
+	// if (!m_font.loadFromFile("assets/font.ttf"))
+	// throw std::runtime_error("Unable to load the font file");
+	
+	// if (!m_logo.loadFromFile("assets/logo.png"))
+	// throw std::runtime_error("Unable to load the logo file");
+	
+	// if (!m_texture.loadFromFile("assets/texture.png"))
+	// throw std::runtime_error("Unable to load the texture file");
+	
+		m_gameStates[GameState::NoCoin] = new NoCoinState(this);
+		m_gameStates[GameState::GetReady] = new GetReadyState(this,m_gameStates[GameState::Playing]);
+		m_gameStates[GameState::Playing] = new PlayingState(this);
+		m_gameStates[GameState::Lost] = new LostState(this,m_gameStates[GameState::Playing]);
+		m_gameStates[GameState::Won] = new WonState(this,m_gameStates[GameState::Playing]);
+		
+		changeGameState(GameState::NoCoin);
+}
+
+Game::~Game()
+{
+	for (GameState* gameState : m_gameStates)
+	delete gameState;
+}
+
+void Game::run()
+{
+	sf::Clock frameClock;
+	while (m_window.isOpen())
+    {
+		while (const std::optional event = m_window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>()) m_window.close();   
+                     if (event->is<sf::Event::KeyPressed>())
+                     {
+						 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::I)) m_currentState->insertCoin();
+						 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))  m_currentState->pressButton();
+						 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) m_currentState->moveStick(sf::Vector2i(-1, 0));
+						 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) m_currentState->moveStick(sf::Vector2i(1, 0));
+						 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) m_currentState->moveStick(sf::Vector2i(0, -1));
+						 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) m_currentState->moveStick(sf::Vector2i(0, 1));
+						
+					 }
+					 
+        }
+        m_currentState->update(frameClock.restart());
+		m_window.clear();
+		m_currentState->draw(m_window);
+        m_window.display();
+    }
+    
+}
+
+void Game::changeGameState(GameState::State gameState)
+{
+	   m_currentState = m_gameStates[gameState];
+}
+
+sf::Font& Game::getFont()
+{
+	return m_font;
+}
+
+sf::Texture& Game::getLogo()
+{
+	return m_logo;
+}
+
+sf::Texture& Game::getTexture()
+{
+	return m_texture;
+}
